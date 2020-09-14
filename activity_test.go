8673758b3a92578cb6665bb7490357564943f777
ECole <eddie.cole.nh@gmail.com>
2020-09-14 15:51:36 -0400

    Update activity_test.go

--- activity_test.go
+++ activity_test.go
@@ -20,16 +20,11 @@ func TestEval(t *testing.T) {
 
 	act := &Activity{}
 	tc := test.NewActivityContext(act.Metadata())
-	input := &Input{AnInput: "test"}
+	input := &Input{Message: "test"}
 	err := tc.SetInputObject(input)
 	assert.Nil(t, err)
 
 	done, err := act.Eval(tc)
 	assert.True(t, done)
 	assert.Nil(t, err)
-
-	output := &Output{}
-	err = tc.GetOutputObject(output)
-	assert.Nil(t, err)
-	assert.Equal(t, "test", output.AnOutput)
 }
